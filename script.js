d898b42f2fe9aeed705bfbd67799da894796db1d
Atsisey Emmanuel <emmatsisey7@gmail.com>
2026-07-25 20:44:48 +0000

    DE4ULT website first version

---
+++ script.js
@@ -0,0 +1,13 @@
+// DE4ULT COIN Website Script
+
+console.log("DE4ULT COIN website loaded successfully");
+
+
+const launchButton = document.querySelector("button");
+
+
+launchButton.addEventListener("click", function(){
+
+    alert("DE4ULT COIN App Launching Soon 🚀");
+
+});
